a57314431ab2697b316c2126c7b802e2047a0b49
Youngeui Hong <snuhongfrcs@snu.ac.kr>
2024-01-17 00:51:23 +0900

    Refactor: _app.tsx의 QueryClient에서 default query function 삭제 / layout.tsx와 동일하게 css 수정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,18 +5,10 @@ import {
   QueryClientProvider,
 } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import axios from "axios";
 import AuthContext from "@/components/provider/AuthProvider";
 import Header from "@/components/header/Header";
 import { Grid } from "@mui/material";
 import CustomThemeProvider from "@/components/theme/ThemeProvider";
-
-const defaultQueryFn = async ({ queryKey }) => {
-  const { data } = await axios.get(
-    `https://jsonplaceholder.typicode.com${queryKey[0]}`,
-  );
-  return data;
-};
 
 export default function MyApp({ Component, pageProps }) {
   const [queryClient] = React.useState(
@@ -24,7 +16,6 @@ export default function MyApp({ Component, pageProps }) {
       new QueryClient({
         defaultOptions: {
           queries: {
-            queryFn: defaultQueryFn,
             // With SSR, we usually want to set some default staleTime
             // above 0 to avoid refetching immediately on the client
             staleTime: 60 * 1000,
@@ -33,32 +24,33 @@ export default function MyApp({ Component, pageProps }) {
       }),
   );
 
-  // provide the default query function to your app with defaultOptions
   return (
-    <AuthContext>
-      <CustomThemeProvider>
-        <QueryClientProvider client={queryClient}>
-          <HydrationBoundary state={pageProps.dehydratedState}>
-            <Header />
-            <Grid
-              container
-              direction="row"
-              justifyContent="center"
-              alignItems="center"
-              maxWidth="xl"
-              sx={{
-                mx: "auto",
-                backgroundColor: "#F8F8F8",
-                paddingTop: 10,
-                minHeight: "98vh",
-              }}
-            >
-              <Component {...pageProps} />
-            </Grid>
-          </HydrationBoundary>
-          <ReactQueryDevtools initialIsOpen={true} />
-        </QueryClientProvider>
-      </CustomThemeProvider>
-    </AuthContext>
+    <div style={{ backgroundColor: "#F8F8F8" }}>
+      <AuthContext>
+        <CustomThemeProvider>
+          <QueryClientProvider client={queryClient}>
+            <HydrationBoundary state={pageProps.dehydratedState}>
+              <Header />
+              <Grid
+                container
+                direction="row"
+                justifyContent="center"
+                alignItems="center"
+                maxWidth="xl"
+                sx={{
+                  mx: "auto",
+                  width: "100%",
+                  paddingTop: 10,
+                  minHeight: "98vh",
+                }}
+              >
+                <Component {...pageProps} />
+              </Grid>
+            </HydrationBoundary>
+            <ReactQueryDevtools initialIsOpen={true} />
+          </QueryClientProvider>
+        </CustomThemeProvider>
+      </AuthContext>
+    </div>
   );
 }
